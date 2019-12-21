69476f45426d6e68825da85b9f724b23f72cef03
Chou Cheng Lei <chenglei.chou@gmail.com>
2019-12-22 01:18:41 +0800

    Commit build folder

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/vuefirebase/' :
-        '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/vueinstagram/' : '/'
 
-    "transpileDependencies": [
-        "vuetify"
-    ]
+    // "transpileDependencies": [
+    //     "vuetify"
+    // ]
 }
