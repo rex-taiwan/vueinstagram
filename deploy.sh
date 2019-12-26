72228523d829d298a54c904a2d76c1fb2a471b24
Chou Cheng Lei <chenglei.chou@gmail.com>
2019-12-26 19:47:21 +0800

    Update lazy src caption,Upload App logo, Application Title

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ rm -rf ./dist
 npm run build
 
 git add .
-git commit -m "Upload App logo, Application Title"
+git commit -m "Update lazy src caption,Upload App logo, Application Title"
 git push
 
 git subtree push --prefix dist origin gh-pages
