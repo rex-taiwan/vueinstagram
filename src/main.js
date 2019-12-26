cef98e988e666fddede0a2801737044a74e8e33e
Chou Cheng Lei <chenglei.chou@gmail.com>
2019-12-26 19:57:38 +0800

    Update lazy src caption,Upload App logo, Application Title

--- src/main.js
+++ src/main.js
@@ -10,8 +10,7 @@ import * as VueFire from 'vuefire';
 Vue.use(VueFire)
 import ReadMore from 'vue-read-more';
 Vue.use(ReadMore);
-import VueDragscroll from "vue-dragscroll";
-Vue.use(VueDragscroll);
+
 
 
 Vue.config.productionTip = false
